18a8a7746efb0c7de70213b54299000416b594e6
chandru <chandru@fytechno.com>
2021-11-11 15:01:30 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,39 @@
+<?php
+$title='About';
+include 'head.php';
+
+?>
+<div class="breadcrumbs">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<p class="bread"><span><a href="index.php">Home</a></span> / <span>About</span></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="colorlib-about">
+			<div class="container">
+				<div class="row row-pb-lg">
+					<div class="col-sm-6 mb-3">
+						<div class="video colorlib-video" style="background-image: url(images/about.jpg);">
+							<a href="https://vimeo.com/channels/staffpicks/93951774" class="popup-vimeo"><i class="icon-play3"></i></a>
+							<div class="overlay"></div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="about-wrap">
+							<h4>
+“Purni Bags N More” is ultimate shopping destination for Lifestyle products.</h4>
+							<p>Since 2021, we’ve built a community that connects customers and consumers to find the products they are looking for. Shopping on Purni Bags N More multiple products to choose, we do B2B wholesale, Consumer and customers a fast, easy and secure way to shop through Cash & Carry.</p>
+							<p>Begin your journey online with one of the fastest growing, e-commerce brands and sourced straight from the best brands. E-commerce shopping experience in the joy of through the best Indian and International brands in lifestyle products</p>
+							<p>Shop everything for your life today and make <strong>Purni Bags N More</strong> a part of your lifestyle.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<?php 
+include 'footer.php';
---
+++ list-products.php
@@ -0,0 +1,335 @@
+<?php
+$title='Product List';
+include 'head.php';
+
+?>
+
+<div class="breadcrumbs">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<p class="bread"><span><a href="index.php">Home</a></span> / <span>Product Details</span></p>
+					</div>
+				</div>
+			</div>
+		</div>
+<div class="colorlib-product">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-xl-3">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="side border mb-1">
+									<h3>Shop by Category</h3>
+									<ul>
+										<li><a href="list-products.php">Corporate Gifts</a></li>
+										<li><a href="list-products.php">Electronics Accessories</a></li>
+										<li><a href="list-products.php">Travel Bag</a></li>
+										<li><a href="list-products.php">Travel Accessories</a></li>
+										<li><a href="list-products.php">Trucking Bags</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-sm-12 d-none">
+								<div class="side border mb-1">
+									<h3>Size/Width</h3>
+									<div class="block-26 mb-2">
+										<h4>Size</h4>
+					               <ul>
+					                  <li><a href="product-detail.php">7</a></li>
+					                  <li><a href="product-detail.php">7.5</a></li>
+					                  <li><a href="product-detail.php">8</a></li>
+					                  <li><a href="product-detail.php">8.5</a></li>
+					                  <li><a href="product-detail.php">9</a></li>
+					                  <li><a href="product-detail.php">9.5</a></li>
+					                  <li><a href="product-detail.php">10</a></li>
+					                  <li><a href="product-detail.php">10.5</a></li>
+					                  <li><a href="product-detail.php">11</a></li>
+					                  <li><a href="product-detail.php">11.5</a></li>
+					                  <li><a href="product-detail.php">12</a></li>
+					                  <li><a href="product-detail.php">12.5</a></li>
+					                  <li><a href="product-detail.php">13</a></li>
+					                  <li><a href="product-detail.php">13.5</a></li>
+					                  <li><a href="product-detail.php">14</a></li>
+					               </ul>
+					            </div>
+					            <div class="block-26 d-none">
+										<h4>Width</h4>
+					               <ul>
+					                  <li><a href="product-detail.php">M</a></li>
+					                  <li><a href="product-detail.php">W</a></li>
+					               </ul>
+					            </div>
+								</div>
+							</div>
+							<div class="col-sm-12 d-none">
+								<div class="side border mb-1">
+									<h3>Style</h3>
+									<ul>
+										<li><a href="product-detail.php">Slip Ons</a></li>
+										<li><a href="product-detail.php">Boots</a></li>
+										<li><a href="product-detail.php">Sandals</a></li>
+										<li><a href="product-detail.php">Lace Ups</a></li>
+										<li><a href="product-detail.php">Oxfords</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-sm-12 d-none">
+								<div class="side border mb-1">
+									<h3>Colors</h3>
+									<ul>
+										<li><a href="product-detail.php">Black</a></li>
+										<li><a href="product-detail.php">White</a></li>
+										<li><a href="product-detail.php">Blue</a></li>
+										<li><a href="product-detail.php">Red</a></li>
+										<li><a href="product-detail.php">Green</a></li>
+										<li><a href="product-detail.php">Grey</a></li>
+										<li><a href="product-detail.php">Orange</a></li>
+										<li><a href="product-detail.php">Cream</a></li>
+										<li><a href="product-detail.php">Brown</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-sm-12 d-none">
+								<div class="side border mb-1">
+									<h3>Material</h3>
+									<ul>
+										<li><a href="product-detail.php">Leather</a></li>
+										<li><a href="product-detail.php">Suede</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-sm-12 d-none">
+								<div class="side border mb-1">
+									<h3>Technologies</h3>
+									<ul>
+										<li><a href="product-detail.php">BioBevel</a></li>
+										<li><a href="product-detail.php">Groove</a></li>
+										<li><a href="product-detail.php">FlexBevel</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-9 col-xl-9">
+						<div class="row row-pb-md">
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-1.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-2.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Minam Meaghan</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-3.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Men's Taja Commissioner</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="w-100"></div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-4.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Russ Men's Sneakers</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-5.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-6.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="w-100"></div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-7.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-8.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-9.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="w-100"></div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-10.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-11.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-12.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="w-100"></div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-13.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-14.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mb-4 text-center">
+								<div class="product-entry border">
+									<a href="product-detail.php" class="prod-img">
+										<img src="images/item-15.jpg" class="img-fluid" alt="Free html5 bootstrap 4 template">
+									</a>
+									<div class="desc">
+										<h2><a href="product-detail.php">Women's Boots Shoes Maca</a></h2>
+										<span class="price">$139.00</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<div class="block-27">
+				               <ul>
+					               <li><a href="product-detail.php"><i class="ion-ios-arrow-back"></i></a></li>
+				                  <li class="active"><span>1</span></li>
+				                  <li><a href="product-detail.php">2</a></li>
+				                  <li><a href="product-detail.php">3</a></li>
+				                  <li><a href="product-detail.php">4</a></li>
+				                  <li><a href="product-detail.php">5</a></li>
+				                  <li><a href="product-detail.php"><i class="ion-ios-arrow-forward"></i></a></li>
+				               </ul>
+				            </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="colorlib-partner">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 offset-sm-2 text-center colorlib-heading colorlib-heading-sm">
+						<h2>Trusted Partners</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col partner-col text-center">
+						<img src="images/brand-1.jpg" class="img-fluid" alt="Free html4 bootstrap 4 template">
+					</div>
+					<div class="col partner-col text-center">
+						<img src="images/brand-2.jpg" class="img-fluid" alt="Free html4 bootstrap 4 template">
+					</div>
+					<div class="col partner-col text-center">
+						<img src="images/brand-3.jpg" class="img-fluid" alt="Free html4 bootstrap 4 template">
+					</div>
+					<div class="col partner-col text-center">
+						<img src="images/brand-4.jpg" class="img-fluid" alt="Free html4 bootstrap 4 template">
+					</div>
+					<div class="col partner-col text-center">
+						<img src="images/brand-5.jpg" class="img-fluid" alt="Free html4 bootstrap 4 template">
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+<?php 
+include 'footer.php';
